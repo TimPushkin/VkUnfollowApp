8c73c50c9c7057c620e5a3fc410d2ae33167aea3
TimPushkin <pushkin.td@gmail.com>
2022-03-30 20:37:26 +0300

    Add Vk authorization

--- app/src/main/java/me/timpushkin/vkunsubapp/MainActivity.kt
+++ app/src/main/java/me/timpushkin/vkunsubapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package me.timpushkin.vkunsubapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,11 +10,25 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 import me.timpushkin.vkunsubapp.ui.theme.VkUnsubAppTheme
 
+private const val TAG = "MainActivity"
+
 class MainActivity : ComponentActivity() {
+    private val authLauncher = VK.login(this) { result ->
+        when (result) {
+            is VKAuthenticationResult.Success -> Log.i(TAG, "Login succeeded")
+            is VKAuthenticationResult.Failed -> Log.i(TAG, "Login failed")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        authLauncher.launch(listOf(VKScope.GROUPS))
 
         setContent {
             VkUnsubAppTheme {
