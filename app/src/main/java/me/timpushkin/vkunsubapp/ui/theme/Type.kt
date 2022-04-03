056d7112789cd6236724ef715005c6f582938e38
TimPushkin <pushkin.td@gmail.com>
2022-04-03 22:12:18 +0300

    Remove VkSans

--- app/src/main/java/me/timpushkin/vkunsubapp/ui/theme/Type.kt
+++ app/src/main/java/me/timpushkin/vkunsubapp/ui/theme/Type.kt
@@ -2,20 +2,14 @@ package me.timpushkin.vkunsubapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import me.timpushkin.vkunsubapp.R
-
-val vkSans = FontFamily(
-    Font(R.font.vksans_display_medium, weight = FontWeight.Medium)
-)
 
 val Typography = Typography(
     h6 = TextStyle(
-        fontFamily = vkSans,
-        fontWeight = FontWeight.Medium,
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Bold,
         fontSize = 20.sp
     ),
     body1 = TextStyle(
